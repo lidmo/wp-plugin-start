952ae2e3b31f106042af84de9ce1f5f1b3e63bce
Wesley Silva <wesleysilva@lidmo.com.br>
2023-02-21 17:58:57 -0300

    adicionando o nome do plugin dinamicamente

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-$plugin = new \Lidmo\WP\Foundation\Plugin('lidmo', __FILE__);
+$plugin = new \Lidmo\WP\Foundation\Plugin(str_replace(WP_PLUGIN_DIR . '/', '', dirname(__FILE__)), __FILE__);
 
 $plugin->singleton(\Lidmo\WP\Foundation\Contracts\Kernel::class, \Lidmo\Hooks\Kernel::class);
 
